e0b49cee4233b63748d5d83151bee1c83d70e8be
YOUR NAME <tmhennis@gmail.com>
2016-10-11 16:19:51 +0200

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :rooms, dependent: :destroy
+  has_one :profile
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_profile?
+    profile.present?
+  end
+
+  def full_name
+    profile.full_name
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :rooms
+  resources :profiles, only: [:new, :edit, :create, :update]
 end

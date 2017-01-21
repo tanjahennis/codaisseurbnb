d50b45621b09f8274872b4569f4a8f3b17645cf4
Tanja Hennis <tmhennis@gmail.com>
2017-01-21 11:58:27 +0100

    serializer room

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -3,30 +3,23 @@ class Api::RoomsController < ApplicationController
 
 
   def index
-    render status: 200, json: {
-        rooms: Room.all
-    }.to_json
+    rooms = Room.all
+    render status: 200, json: rooms
   end
 
   def show
     room = Room.find(params[:id])
-
-    render status: 200, json: {
-        room: room
-    }.to_json
+    render status: 200, json: room
   end
 
   def create
     room = Room.new(room_params)
 
     if room.save
-      render status: 200, json: {
-          message: "Room successfully created",
-          room: room
-      }.to_json
+      render status: 200, json: room
     else
       render status: 422, json: {
-          errors: room.errors
+        errors: room.errors
       }.to_json
     end
   end
@@ -36,25 +29,22 @@ class Api::RoomsController < ApplicationController
     room.destroy
 
     render status: 200, json: {
-        message: "Room successfully deleted"
+      message: "Room successfully deleted"
     }.to_json
   end
 
   def update
     room = Room.find(params[:id])
+
     if room.update(room_params)
-      render status: 200, json: {
-          message: "Room successfully updated",
-          room: room
-      }.to_json
+      render status: 200, json: room
     else
-      render status: 422, json: {
-          message: "The room could not be updated",
-          errors: room.errors
+      render status: 500, json: {
+        message: "The room could not be updated",
+        errors: room.errors
       }.to_json
     end
   end
-
 
   private
 
